16b621c822cdb5d87f6eff4dd844a3ce3b19dba5
byeron <me@lab>
2024-06-13 14:36:37 +0900

    add: ---
    <body: why>
    
    ---
    add: (機能・ファイルを)追加する
    
    fix: (コードを)修正する
    
    clean: (コードを)改善する; 名前の変更/コードの整形
    
    update: (パッケージやドキュメントを)更新する
    
    remove: (ファイルやコードを)取り除く
    
    rename: (ファイル名を)変更する
    
    change: (AをBに)変更する

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as stats
 
 
-def standardize(data):
-    mean = np.mean(data)
-    std = np.std(data)
+def standardize(data, mean=None, std=None):
+    if mean is None:
+        mean = np.mean(data)
+    if std is None:
+        std = np.std(data)
     return (data - mean) / std
 
 
@@ -14,29 +18,124 @@ def chi2(data):
     return _sum
 
 
+def case1(base_data, size, n_samples, mu, sigma):
+    chi2s = []
+    for i in range(n_samples):
+        data = np.random.choice(base_data, size=size)
+        print(data)
+        data = standardize(data, mu, sigma)  # 既知の母平均と母標準偏差を使って標準化
+        print(data)
+        chi2_value = chi2(data)
+        chi2s.append(chi2_value)
+        print(chi2_value)
+    return chi2s
+
+
+def case2(base_data, size, n_samples, sigma):
+    chi2s = []
+    for i in range(n_samples):
+        data = np.random.choice(base_data, size=size)
+        data = standardize(data, std=sigma)  # 母平均を使わずに標準化
+        chi2_value = chi2(data)
+        chi2s.append(chi2_value)
+    return chi2s
+
+
+def case3(base_data, size, n_samples, sigma):
+    chi2s = []
+    for i in range(n_samples):
+        dof = size - 1
+
+        data = np.random.choice(base_data, size=size)
+        chi2_value = (dof * np.var(data, ddof=1)) / (sigma ** 2)
+        chi2s.append(chi2_value)
+    return chi2s
+
+
+def case4(base_data, size, n_samples, sigma):
+    chi2s = []
+    for i in range(n_samples):
+        data = np.random.choice(base_data, size=size)
+        chi2_value = size * np.var(data) / (sigma ** 2)
+        chi2s.append(chi2_value)
+    return chi2s
+
+
+def plot(histgram, ax):
+    ax.hist(histgram, bins=50, color='blue', alpha=0.7)
+    ax.set_ylim(0, 200)
+
+
 if __name__ == '__main__':
     np.random.seed(0)
 
-    size = 3
+    mu = 0
+    sigma = 10
+    base_data = np.random.normal(mu, sigma, 90000)
+    size = 100
+    n_samples = 1000
+    fig = plt.figure(figsize=(6, 9))
+
+    # 母平均が既知の場合
+    chi2s = case1(
+        base_data,
+        size=size,
+        n_samples=n_samples,
+        mu=mu,
+        sigma=sigma,
+    )
+
+    # histgram
+    ax = fig.add_subplot(411)
+    plot(chi2s, ax)
+
+    # 母平均が未知の場合
+    # 自由度がn-1のカイ二乗分布に従う
+    chi2s = case2(
+        base_data,
+        size=size,
+        n_samples=n_samples,
+        sigma=sigma,
+    )
+
+    # histgram
+    ax = fig.add_subplot(412)
+    plot(chi2s, ax)
+
+    # 不偏分散を使った場合
+    chi2s = case3(
+        base_data,
+        size=size,
+        n_samples=n_samples,
+        sigma=sigma,
+    )
+    # histgram
+    ax = fig.add_subplot(413)
+    plot(chi2s, ax)
+
+    # 標本分散を使った場合
+    chi2s = case4(
+        base_data,
+        size=size,
+        n_samples=n_samples,
+        sigma=sigma,
+    )
+
+    # histgram
+    ax = fig.add_subplot(414)
+    plot(chi2s, ax)
+    plt.tight_layout()
+    fig.savefig('hist.png')
+
+    # 区間推定してみる
+    alpha = 0.05
     dof = size - 1
+    lower = stats.chi2.ppf(alpha / 2, dof)
+    upper = stats.chi2.ppf(1 - alpha / 2, dof)
+    lower = (dof * np.var(base_data, ddof=1)) / lower
+    upper = (dof * np.var(base_data, ddof=1)) / upper
 
-    # 母分散が等しい2群のデータを生成
-    base_data = np.random.normal(0, 10, size)
-    same_var = np.random.normal(5, 10, size)
-
-    # 母分散が異なる2群のデータを生成
-    diff_var = np.random.normal(0, 20, size)
-
-    # chi^2 統計量を計算, 標準化なし
-    # 標準化しないと、母分散が同じでも異なる値になる
-    base_chi2 = chi2(base_data)
-    same_var_chi2 = chi2(same_var)
-    print(base_chi2)
-    print(same_var_chi2)
-
-    # chi^2 統計量を計算, 標準化あり
-    # 標準化すると、母分散が同じなら近い値になる
-    base_chi2 = chi2(standardize(base_data))
-    same_var_chi2 = chi2(standardize(same_var))
-    print(base_chi2)
-    print(same_var_chi2)
+    # standard deviation
+    lower = np.sqrt(lower)
+    upper = np.sqrt(upper)
+    print(upper, lower)
